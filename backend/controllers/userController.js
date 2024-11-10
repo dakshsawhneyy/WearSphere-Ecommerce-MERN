283bb4b6fb67fcd787eed8bb120fa8081b8da6e9
unknown <darkstardaksh@gmail.com>
2024-11-10 12:13:53 +0530

    Login user made

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,64 +1,85 @@
 import validator from "validator";
 import userModel from "../models/userModel.js";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 {/* To create token we need to create an arrow function */}
 const createToken = (id) => {
     // Here we will return token which will be created by jwt
-    return jwt.sign({id},process.env.JWT_SECRET)
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 // Route for login user
-const loginUser = async(req,res) => {
-
-}
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Find email is available in userModel
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exists" });
+        }
+        // If id matched then compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id);
+            res.json({ success: true, token });
+        } else {
+            res.json({ success: false, msg: "Invalid Credentials" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, msg: error.message });
+    }
+};
 
 // Route for signup user
-const registerUser = async(req,res) => {
+const registerUser = async (req, res) => {
     try {
-        const {name,email,password} = req.body;
+        const { name, email, password } = req.body;
 
         // Checking user alreasy exists or not
-        const exists = await userModel.findOne({email})
-        if(exists) {
-            return res.json({success:false,msg:"User Already Exists"})
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, msg: "User Already Exists" });
         }
 
         // Validating email format and strong password using validator
-        if(!validator.isEmail(email)) {
-            return res.json({success:false,msg:"Please enter a valid email"})
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                msg: "Please enter a valid email",
+            });
         }
-        if(password.length < 6) {
-            return res.json({success:false,msg:"Please enter a password of atleast 6 characters"})
+        if (password.length < 6) {
+            return res.json({
+                success: false,
+                msg: "Please enter a password of atleast 6 characters",
+            });
         }
 
         // Now when email is also valid and it is unique so then we have to store email and password and store password in hash form and encrypted
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(password,salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
 
         const newUser = new userModel({
             name,
             email,
-            password:hashPassword
-        })
+            password: hashPassword,
+        });
 
         const user = newUser.save();
 
         // Whenever there will be a new user created. A unique token as its id is made
-        const token = createToken(user._id)
+        const token = createToken(user._id);
 
-        res.json({success:true,token})
-
+        res.json({ success: true, token });
     } catch (error) {
-        console.log(error)
-        res.json({success:false,msg:"Error Occurred"})
+        console.log(error);
+        res.json({ success: false, msg: error.message });
     }
-}
+};
 
 // Route for admin login
-const adminLogin = async(req,res) => {
+const adminLogin = async (req, res) => {};
 
-}
-
-export { loginUser,registerUser,adminLogin }
+export { loginUser, registerUser, adminLogin };
