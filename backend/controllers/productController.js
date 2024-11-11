aedd1e7fd5d8da09ccbb6ef946237d6d1a1aefcf
unknown <darkstardaksh@gmail.com>
2024-11-11 13:10:22 +0530

    product controller made

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+
+// function to add product
+const addProduct = (req,res) => {
+
+}
+
+// function to list products
+const listProducts = (req,res) => {
+
+}
+
+// function to remove product
+const removeProduct = (req,res) => {
+
+}
+
+// function for single product info
+const singleProduct = () => {
+
+}
+
+export { listProducts,addProduct,removeProduct,singleProduct };
