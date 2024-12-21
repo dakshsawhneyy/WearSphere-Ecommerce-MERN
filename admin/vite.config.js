cd82ca92ac2a45948f49bbf7e68474cf26a79de2
unknown <darkstardaksh@gmail.com>
2024-12-21 11:53:46 +0530

    Admin Created through vite

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: {port : 5173}
+  server:{port: 5174} // Added Port number so that frontend and backend not clash
 })
