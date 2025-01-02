eb01f70fb5839abf8219e6cd43e98f441acc6737
unknown <darkstardaksh@gmail.com>
2025-01-02 15:27:24 +0530

    Written logic for list items

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { backend_url } from '../App'
 
 const List = () => {
+    
+    // First we need to get data from api and store it in state variable
+    const [list, setList] = useState([])
+
+    // We will run this fxn whenever this page is loaded to get all the items by calling the api
+    const fetchList = async() => {
+        try {
+            const response = await axios.get(backend_url + `/api/product/list`)
+            if (response.data.success) {
+                setList(response.data.products)
+            } else {
+                toast.error(response.data.message)
+            }
+            //console.log(response.data)
+        } catch (error) {
+            toast.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchList();
+    }, [])
+
 return (
-    <div>
+    <>
         
-    </div>
+    </>
 )
 }
 
