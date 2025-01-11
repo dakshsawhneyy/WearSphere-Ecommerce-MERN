18b794b8caa5df866fca4897fff5bb1a76f03521
unknown <darkstardaksh@gmail.com>
2025-01-11 15:21:17 +0530

    MAde orderRouter.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,33 @@
-import orderModel from "../models/orderModel";
+import orderModel from "../models/orderModel.js";
 
 // Placing orders using COD
+const placeOrderCOD = async(req,res) => {
+
+}
+
+// Placing orders using Stripe
+const placeOrderStripe = async(req,res) => {
+
+}
+
+// Placing orders using RazerPay
+const placeOrderRazorpay = async(req,res) => {
+
+}
+
+// All Orders data for admin panel
+const allOrders = async(req,res) => {
+
+}
+
+// User Order data for frontend
+const userOrders = async(req,res) => {
+
+}
+
+// update order status from admin panel. Only admin can update the status
+const updateStatus = async(req,res) => {
+
+}
+
+export { placeOrderCOD,placeOrderRazorpay,placeOrderStripe,allOrders,userOrders,updateStatus }
---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { placeOrderCOD,placeOrderStripe,placeOrderRazorpay,updateStatus,userOrders,allOrders } from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+
+
+const orderRouter = express.Router();
+
+// Admin Features
+orderRouter.post('/list',adminAuth,allOrders)  // Admin panel list all orders
+orderRouter.post('/status',adminAuth,updateStatus)  // Admin panel update orders
+
+// Payment Features
+orderRouter.post('/place',authUser,placeOrderCOD)
+orderRouter.post('/stripe',authUser,placeOrderStripe)   // we need token/userId so we gonna need auth.js middleware
+orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+
+// User features - frontend
+orderRouter.post('/userOrders',authUser,userOrders)
+
+export default orderRouter;
