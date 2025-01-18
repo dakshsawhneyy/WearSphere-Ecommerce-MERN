b39525516f055fcf0caf2f7671e25b322c2c8e25
unknown <darkstardaksh@gmail.com>
2025-01-18 19:42:31 +0530

    Error Coorected

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSellers from '../components/BestSellers'
-import OurPolicy from '../components/Ourpolicy'
+import OurPolicy from '../components/OurPolicy'
 import SubscribeDiscount from '../components/SubscribeDiscount'
 
 const Home = () => {
