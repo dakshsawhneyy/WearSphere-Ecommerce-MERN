d27c9b3736c01e198c10fa55222c7e0ee1db2bb6
Ubuntu <ubuntu@ip-172-31-32-244.ap-south-1.compute.internal>
2025-04-20 10:36:47 +0000

    Added Docker-compose file

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-	port: 5174,
-	host: '0.0.0.0'
-  } // Added Port number so that frontend and backend not clash
-})
+  server: {
+    host: '0.0.0.0',  // This ensures Vite listens on all network interfaces
+    port: 5174,
+    strictPort: true
+  },
+});
+
