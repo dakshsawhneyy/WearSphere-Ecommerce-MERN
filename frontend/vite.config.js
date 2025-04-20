c7889707c7f935723426f6e2a09ebc9b8dbccbdb
Ubuntu <ubuntu@ip-172-31-32-244.ap-south-1.compute.internal>
2025-04-20 03:54:41 +0000

    Added Dockerfile for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: {port : 5173}
-})
+  server: {
+    host: '0.0.0.0',  // This ensures Vite listens on all network interfaces
+    port: 5173         // This specifies the port to be used
+  }
+});
